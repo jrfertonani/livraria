ac87fd219563706d2e55680ccec50a5050457be7
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-17 17:29:23 -0300

    Livros entity

---
+++ Back/src/main/java/Back/livros/model/entity/Livros.java
@@ -0,0 +1,21 @@
+package Back.livros.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data   @Entity
+public class Livros implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+
+}
