4da31607d7b597386a1f82469b4bb8e9533f6a8a
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-17 15:45:11 -0300

    usuarios entity

---
+++ Back/src/main/java/Back/usuarios/model/entity/Usuarios.java
@@ -0,0 +1,22 @@
+package Back.usuarios.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data   @Entity
+public class Usuarios implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+}
