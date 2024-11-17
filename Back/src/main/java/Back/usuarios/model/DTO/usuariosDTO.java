df78c7ede4619e93f6a2cb809659775da78dd256
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-17 15:45:23 -0300

    usuarios DTO

---
+++ Back/src/main/java/Back/usuarios/model/DTO/usuariosDTO.java
@@ -0,0 +1,13 @@
+package Back.usuarios.model.DTO;
+
+import lombok.Data;
+
+@Data
+public class usuariosDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+}
