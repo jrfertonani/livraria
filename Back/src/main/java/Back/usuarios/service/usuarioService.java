46c5951f408a452761a5bb9702b5f1818424c058
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-17 16:38:52 -0300

    usuarios service

--- Back/src/main/java/Back/usuarios/service/usuarioService.java
+++ Back/src/main/java/Back/usuarios/service/usuarioService.java
@@ -1,9 +1,14 @@
 package Back.usuarios.service;
 
+import Back._config.Exeptions.serviceExceptions.ObjectNotFoundException;
+import Back.usuarios.model.DTO.usuariosDTO;
+import Back.usuarios.model.entity.Usuarios;
 import Back.usuarios.repository.usuarioRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class usuarioService {
@@ -14,4 +19,37 @@ public class usuarioService {
 
     @Autowired
     private usuarioRepository repository;
+
+    public Usuarios create(usuariosDTO DTO) {
+        return repository.save(
+                mapper.map(DTO, Usuarios.class));
+    }
+
+    public List<Usuarios> list() {
+        return repository.findAll();
+    }
+
+    public usuariosDTO findById(Long id){
+        return mapper.map(
+                repository.findById(id)
+                        .orElseThrow(
+                       () -> new ObjectNotFoundException("Objeto não encontrado.  ID: " +id)
+                        ),usuariosDTO.class);
+    }
+
+    public Usuarios update(Long id, usuariosDTO DTO){
+        findById(id);
+        return  repository.save(
+                mapper.map(DTO, Usuarios.class)
+        );
+    }
+
+
+    public void delete(Long id) {
+        findById(id);
+        repository.deleteById(id);
+    }
+
+
+
 }
