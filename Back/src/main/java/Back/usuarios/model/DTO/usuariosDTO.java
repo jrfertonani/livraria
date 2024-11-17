adedd262162d686366598868e56504c475a23b42
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-17 16:38:44 -0300

    usuarios DTO

--- Back/src/main/java/Back/usuarios/model/DTO/usuariosDTO.java
+++ Back/src/main/java/Back/usuarios/model/DTO/usuariosDTO.java
@@ -2,12 +2,15 @@ package Back.usuarios.model.DTO;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class usuariosDTO {
+public class usuariosDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private Long id;
     private String nome;
     private String email;
-    private String telefone;
+    private Long telefone;
 
 }
