ccff9e8dfaa5737f4ed7221ab8fe9870af0489eb
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-18 14:12:35 -0300

    emp-form components

---
+++ Front/src/app/components/emprestimos/emp-form/emp-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './emp-form.component.html',
+  styleUrl: './emp-form.component.css'
+})
+export class EmpFormComponent {
+
+}
