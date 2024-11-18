65c98b188837f7117390c496441af83369744b46
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-18 15:29:49 -0300

    enviroment service

--- Front/src/app/services/livros/livros.service.ts
+++ Front/src/app/services/livros/livros.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LivrosService {
 
-  constructor() { }
+  URL = environment.URL;
+
+  constructor(private http: HttpClient) { }
+
+
+
+
+
 }
