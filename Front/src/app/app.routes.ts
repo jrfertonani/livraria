302a133eaa8550f771654d66c80bdd8b7dd98d79
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-18 14:27:59 -0300

    routes

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 
 
 import { HomeComponent } from './pages/home/home.component';
+import { LivroListComponent } from './components/livros/livro-list/livro-list.component';
+import { UsuarioListComponent } from './components/usuarios/usuario-list/usuario-list.component';
+import { EmpListComponent } from './components/emprestimos/emp-list/emp-list.component';
 
 export const routes: Routes = [
 
   {path:'', component: HomeComponent},
+  {path:'livros', component: LivroListComponent},
+  {path:'usuarios', component: UsuarioListComponent},
+  {path:"emprestimos", component: EmpListComponent}
 
 ];
