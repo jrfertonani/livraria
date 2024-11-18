ffa77ba64fb03a011ba506fa0716cbabf93c0e52
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-18 19:45:11 -0300

    livro create components

---
+++ Front/src/app/components/livros/livro-create/livro-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './livro-create.component.html',
+  styleUrl: './livro-create.component.css'
+})
+export class LivroCreateComponent {
+
+}
