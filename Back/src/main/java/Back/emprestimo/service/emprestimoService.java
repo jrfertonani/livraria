f6a5ee523199d55b335f0327a7a1636ce7fb26c4
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-18 13:06:05 -0300

    emprestimo Servico

--- Back/src/main/java/Back/emprestimo/service/emprestimoService.java
+++ Back/src/main/java/Back/emprestimo/service/emprestimoService.java
@@ -1,11 +1,20 @@
 package Back.emprestimo.service;
 
+import Back._config.Exeptions.serviceExceptions.ObjectNotFoundException;
 import Back.emprestimo.model.DTO.emprestimoDTO;
 import Back.emprestimo.model.entity.Emprestimo;
 import Back.emprestimo.repository.emprestimoRepository;
+import Back.livros.model.DTO.livroDTO;
+import Back.livros.model.entity.Livros;
+import Back.livros.repository.livroRepository;
+import Back.usuarios.model.entity.Usuarios;
+import Back.usuarios.service.usuarioService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class emprestimoService {
@@ -16,12 +25,28 @@ public class emprestimoService {
     @Autowired
     private emprestimoRepository repository;
 
+    @Autowired
+    private livroRepository LivroRepository;
+
+    @Autowired
+    private usuarioService UsuariosService;
+
 
     public Emprestimo create(emprestimoDTO DTO) {
-        return repository.save(
-                mapper.map(DTO, Emprestimo.class)
-        );
+        return repository.save(mapper.map(DTO, Emprestimo.class));
     }
 
+
+    public List<Emprestimo> list() {
+        return repository.findAll();
+    }
+
+    public emprestimoDTO findById(Long id) {
+        return mapper.map(
+        repository.findById(id)
+                .orElseThrow(
+        ()-> new ObjectNotFoundException("Objeto não encontrado! ID: " + id)
+        ), emprestimoDTO.class);
+    }
 
 }
