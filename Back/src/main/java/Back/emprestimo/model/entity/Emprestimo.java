f92fd97e6a0d99fc3c1246635eac9d98317843d4
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-18 10:11:49 -0300

    Emprestimos Entity

---
+++ Back/src/main/java/Back/emprestimo/model/entity/Emprestimo.java
@@ -0,0 +1,39 @@
+package Back.emprestimo.model.entity;
+
+import Back.livros.model.entity.Livros;
+import Back.usuarios.model.entity.Usuarios;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data   @Entity
+public class Emprestimo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_livro")
+    private Livros livro;
+
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuarios usuario;
+
+    @Column(nullable = false)
+    private LocalDate dataEmprestimo;
+
+    @Column(nullable = false)
+    private LocalDate dataDevolucao;
+
+    @Column(nullable = false)
+    private boolean devolvido;
+
+
+
+}
