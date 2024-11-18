35bd6f9bbee9c82809c2e3380b270f5c95eb4bc7
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-18 13:06:30 -0300

    emprestimo DTO

--- Back/src/main/java/Back/emprestimo/model/DTO/emprestimoDTO.java
+++ Back/src/main/java/Back/emprestimo/model/DTO/emprestimoDTO.java
@@ -1,17 +1,21 @@
 package Back.emprestimo.model.DTO;
 
 import java.time.LocalDate;
+
+import Back.livros.model.entity.Livros;
+import Back.usuarios.model.entity.Usuarios;
 import lombok.Data;
 
 @Data
 public class emprestimoDTO {
 
     private Long id;
-    private String tituloLivro; // Em vez de idLivro, exibimos o título do livro
-    private String nomeUsuario; // Em vez de idUsuario, exibimos o nome do usuário
+    private Livros tituloLivro; // Em vez de idLivro, exibimos o título do livro
+    private Usuarios nomeUsuario; // Em vez de idUsuario, exibimos o nome do usuário
     private LocalDate dataEmprestimo;
     private LocalDate dataDevolucao;
     private boolean devolvido;
+
 
 
 }
