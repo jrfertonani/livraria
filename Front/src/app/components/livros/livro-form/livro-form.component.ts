1fea6143762b3a0354174f7e44b5fe5696331422
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-18 14:14:05 -0300

    livro-form components

---
+++ Front/src/app/components/livros/livro-form/livro-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './livro-form.component.html',
+  styleUrl: './livro-form.component.css'
+})
+export class LivroFormComponent {
+
+}
