ec92ee2ee1d37778b0a61c025d13b23981676ed7
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-18 15:50:22 -0300

    livros list

--- Front/src/app/components/livros/livro-list/livro-list.component.ts
+++ Front/src/app/components/livros/livro-list/livro-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LivrosService } from '../../../services/livros/livros.service';
+import { Livros } from '../../../model/Livros';
 
 @Component({
   selector: 'app-livro-list',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './livro-list.component.html',
   styleUrl: './livro-list.component.css'
 })
-export class LivroListComponent {
+export class LivroListComponent implements OnInit{
+
+  livro : Livros [] =[];
+  livrosGeral : Livros [] = [];
+
+  constructor(private serviceLivro: LivrosService){}
+
+
+  ngOnInit(): void {
+    this.serviceLivro.getLivros().subscribe(response =>{
+      this.livro = response.dados;
+    })
+  }
+
+
+
 
 }
