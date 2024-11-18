3057d0a454759ca6f761374929bbabf7e01e0eb9
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-18 14:13:39 -0300

    livro-list components

---
+++ Front/src/app/components/livros/livro-list/livro-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './livro-list.component.html',
+  styleUrl: './livro-list.component.css'
+})
+export class LivroListComponent {
+
+}
