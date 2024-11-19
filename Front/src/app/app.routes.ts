0f382c2ad6a07b89c59a94f8a924e0285854b8b4
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-19 08:36:52 -0300

    routes ts

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -5,12 +5,16 @@ import { HomeComponent } from './pages/home/home.component';
 import { LivroListComponent } from './components/livros/livro-list/livro-list.component';
 import { UsuarioListComponent } from './components/usuarios/usuario-list/usuario-list.component';
 import { EmpListComponent } from './components/emprestimos/emp-list/emp-list.component';
+import { LivroCreateComponent } from './components/livros/livro-create/livro-create.component';
+import { LivroUpdateComponent } from './components/livros/livro-update/livro-update.component';
 
 export const routes: Routes = [
 
   {path:'', component: HomeComponent},
   {path:'livros', component: LivroListComponent},
+  {path:'cadastro',component: LivroCreateComponent},
+  {path:'livros/editar/:id',component: LivroUpdateComponent},
   {path:'usuarios', component: UsuarioListComponent},
-  {path:"emprestimos", component: EmpListComponent}
+  {path:"emprestimos", component: EmpListComponent},
 
 ];
