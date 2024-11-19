d81e9c392bf5a5caa6e48fb129ba7999bd0d2cf6
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-19 08:36:28 -0300

    livro ts

--- Front/src/app/components/livros/livro-create/livro-create.component.ts
+++ Front/src/app/components/livros/livro-create/livro-create.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LivroFormComponent } from "../livro-form/livro-form.component";
 
 @Component({
   selector: 'app-livro-create',
   standalone: true,
-  imports: [],
+  imports: [LivroFormComponent],
   templateUrl: './livro-create.component.html',
   styleUrl: './livro-create.component.css'
 })
