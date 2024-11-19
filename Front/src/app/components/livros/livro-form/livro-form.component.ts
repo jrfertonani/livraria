fef5c10b3e6baaafbecc1d49b9af284d031874c3
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-19 08:59:58 -0300

    livros form HTML

--- Front/src/app/components/livros/livro-form/livro-form.component.ts
+++ Front/src/app/components/livros/livro-form/livro-form.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-livro-form',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './livro-form.component.html',
   styleUrl: './livro-form.component.css'
 })
-export class LivroFormComponent {
+export class LivroFormComponent implements OnInit{
+
+
+  livrosForm!: FormGroup;
+
+  ngOnInit(): void {
+  }
+
+
 
 }
