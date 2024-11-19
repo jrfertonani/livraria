2c5789a7a9d47dc80c0388f8259f97d30d8087ec
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-19 13:09:41 -0300

    implementando elementos basicos para rodar a pagina

--- Front/src/app/components/usuarios/usuario-list/usuario-list.component.ts
+++ Front/src/app/components/usuarios/usuario-list/usuario-list.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Usuarios } from '../../../model/Usuarios';
+import { UsuarioService } from '../../../services/usuarios/usuario.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-list',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './usuario-list.component.html',
   styleUrl: './usuario-list.component.css'
 })
-export class UsuarioListComponent {
+export class UsuarioListComponent implements OnInit{
+
+  usuarios : Usuarios [] =[];
+  usuariosGeral : Usuarios [] = [];
+
+  constructor(private serviceUsuarios: UsuarioService){}
 
 
+  ngOnInit(): void {
+
+    }
+
+
+
+    search(event: Event){}
+
+    deletar(id:number | undefined){}
 }
