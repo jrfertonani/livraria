b8a49a6b28b3225397c7bb6ee223bef46296cc0c
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-19 14:17:58 -0300

    metodo create

--- Front/src/app/components/usuarios/usuario-create/usuario-create.component.ts
+++ Front/src/app/components/usuarios/usuario-create/usuario-create.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioFormComponent } from "../usuario-form/usuario-form.component";
+import { Usuarios } from '../../../model/Usuarios';
+import { UsuarioService } from '../../../services/usuarios/usuario.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-create',
   standalone: true,
-  imports: [],
+  imports: [UsuarioFormComponent],
   templateUrl: './usuario-create.component.html',
   styleUrl: './usuario-create.component.css'
 })
-export class UsuarioCreateComponent {
+export class UsuarioCreateComponent implements OnInit{
+
+
+  constructor(private serviceUsuario: UsuarioService, private router: Router){}
+
+
+  ngOnInit(): void {
+  }
+
+  criarUsuario(usuario: Usuarios){
+    this.serviceUsuario.createUsuario(usuario).subscribe(usuario=>{
+      this.router.navigate(['usuarios'])
+    })
+  }
 
 }
