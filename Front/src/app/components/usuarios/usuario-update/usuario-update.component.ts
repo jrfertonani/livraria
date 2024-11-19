622c255458d4133f701722873f2bc905fe5c80e0
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-19 14:29:42 -0300

    metodo update

--- Front/src/app/components/usuarios/usuario-update/usuario-update.component.ts
+++ Front/src/app/components/usuarios/usuario-update/usuario-update.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioFormComponent } from "../usuario-form/usuario-form.component";
+import { UsuarioService } from '../../../services/usuarios/usuario.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-update',
   standalone: true,
-  imports: [],
+  imports: [UsuarioFormComponent],
   templateUrl: './usuario-update.component.html',
   styleUrl: './usuario-update.component.css'
 })
-export class UsuarioUpdateComponent {
+export class UsuarioUpdateComponent implements OnInit{
+
+  constructor(private serviceUsuario: UsuarioService, private router: Router, private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    const id = Number( this.route.snapshot.paramMap.get('id'))
+
+
+  }
 
 }
