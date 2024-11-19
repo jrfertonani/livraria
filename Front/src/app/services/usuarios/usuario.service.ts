fcc25a734fbbd061ce77735e25a46aca004f7c2a
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-19 13:25:50 -0300

    metodo findAllUsuario

--- Front/src/app/services/usuarios/usuario.service.ts
+++ Front/src/app/services/usuarios/usuario.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuarios } from '../../model/Usuarios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+
+  URL = 'http://localhost:8080/usuarios';
+
+  constructor(private http: HttpClient) { }
+
+
+  findALlUsuario():Observable<Usuarios[]>{
+    return this.http.get<Usuarios[]>(this.URL);
+  }
+
 }
