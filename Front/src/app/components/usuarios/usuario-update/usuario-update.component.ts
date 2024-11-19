e6674b711ded5cb507a5180393ee71151597fa1c
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-19 13:09:03 -0300

    components usuarios update

---
+++ Front/src/app/components/usuarios/usuario-update/usuario-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuario-update.component.html',
+  styleUrl: './usuario-update.component.css'
+})
+export class UsuarioUpdateComponent {
+
+}
