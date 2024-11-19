af924aa383026fd93acd9fb646c8fa15affd99ca
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-19 09:49:47 -0300

    consolidando o metodo getLivrosById

--- Front/src/app/components/livros/livro-update/livro-update.component.ts
+++ Front/src/app/components/livros/livro-update/livro-update.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LivroFormComponent } from "../livro-form/livro-form.component";
 import { LivrosService } from '../../../services/livros/livros.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Livros } from '../../../model/Livros';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-livro-update',
   standalone: true,
-  imports: [LivroFormComponent],
+  imports: [LivroFormComponent, CommonModule],
   templateUrl: './livro-update.component.html',
   styleUrl: './livro-update.component.css'
 })
-export class LivroUpdateComponent {
+export class LivroUpdateComponent implements OnInit{
 
+  livro!: Livros;
 
   constructor(private esrviceLivro: LivrosService, private router: Router, private route: ActivatedRoute){}
+
+
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+
+    this.esrviceLivro.getLivrosById(id).subscribe(livros =>{
+      this.livro = livros;
+    })
+
+
+  }
 
 }
