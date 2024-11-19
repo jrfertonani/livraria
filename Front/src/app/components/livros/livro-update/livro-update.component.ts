6c73e38c6b0b1a438901e670b795d392a3c7fade
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-19 09:33:11 -0300

    implementando router, route

--- Front/src/app/components/livros/livro-update/livro-update.component.ts
+++ Front/src/app/components/livros/livro-update/livro-update.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { LivroFormComponent } from "../livro-form/livro-form.component";
+import { LivrosService } from '../../../services/livros/livros.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-livro-update',
   standalone: true,
-  imports: [],
+  imports: [LivroFormComponent],
   templateUrl: './livro-update.component.html',
   styleUrl: './livro-update.component.css'
 })
 export class LivroUpdateComponent {
+
+
+  constructor(private esrviceLivro: LivrosService, private router: Router, private route: ActivatedRoute){}
 
 }
