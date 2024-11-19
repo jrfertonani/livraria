d716beec827611af0bbdc08f478b26082f79fe29
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-19 14:18:11 -0300

    metodo create

--- Front/src/app/components/usuarios/usuario-form/usuario-form.component.ts
+++ Front/src/app/components/usuarios/usuario-form/usuario-form.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Usuarios } from '../../../model/Usuarios';
 
 @Component({
   selector: 'app-usuario-form',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './usuario-form.component.html',
   styleUrl: './usuario-form.component.css'
 })
-export class UsuarioFormComponent {
+export class UsuarioFormComponent implements OnInit{
+
+  @Output() onSubmit = new EventEmitter<Usuarios>();
+
+
+
+
+  usuariorForm!: FormGroup;
+
+  ngOnInit(): void {
+
+    this.usuariorForm = new FormGroup({
+      id: new FormControl(0),
+      nome: new FormControl('')
+
+    })
+  }
+
+  submit(){
+    this.onSubmit.emit(this.usuariorForm.value);
+  }
 
 }
