e35ddc5eb9118305dbe3882ccd5f1b3ba2772cb5
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-20 14:21:26 -0300

    implementando ts de form

--- Front/src/app/components/emprestimos/emp-form/emp-form.component.ts
+++ Front/src/app/components/emprestimos/emp-form/emp-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-emp-form',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './emp-form.component.html',
   styleUrl: './emp-form.component.css'
 })
