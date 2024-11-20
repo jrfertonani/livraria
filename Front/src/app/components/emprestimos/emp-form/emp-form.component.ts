f13bb5ed2cc5a1bcd4b9239c3fe75bfad0e95fd3
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-20 14:41:02 -0300

    implementando output em form

--- Front/src/app/components/emprestimos/emp-form/emp-form.component.ts
+++ Front/src/app/components/emprestimos/emp-form/emp-form.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Emprestimos } from '../../../model/Emprestimos';
 
 @Component({
   selector: 'app-emp-form',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './emp-form.component.html',
   styleUrl: './emp-form.component.css'
 })
-export class EmpFormComponent {
+export class EmpFormComponent implements OnInit{
+
+  @Output() onSubmit = new EventEmitter<Emprestimos>();
+
+
+  emprestimoForm!: FormGroup;
+
+
+  ngOnInit(): void {
+    this.emprestimoForm = new FormGroup({
+      id : new FormGroup(0),
+      tituloLivro : new FormGroup(''),
+      nomeUsuario : new FormGroup(''),
+      dataEmprestimo : new FormGroup(0),
+      dataDevolucao : new FormGroup(0),
+      devolvido : new FormGroup(false)
+    });
+  }
+
+
+  submit(){
+    this.onSubmit.emit(this.emprestimoForm.value);
+  }
+
+
 
 }
