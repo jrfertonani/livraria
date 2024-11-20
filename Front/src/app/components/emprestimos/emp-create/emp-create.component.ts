e3c7f395b957c12c3050d4e6e98dd018068f5843
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-20 14:22:25 -0300

    implementando page de form dentro de create

--- Front/src/app/components/emprestimos/emp-create/emp-create.component.ts
+++ Front/src/app/components/emprestimos/emp-create/emp-create.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { EmpFormComponent } from "../emp-form/emp-form.component";
 
 @Component({
   selector: 'app-emp-create',
   standalone: true,
-  imports: [],
+  imports: [EmpFormComponent],
   templateUrl: './emp-create.component.html',
   styleUrl: './emp-create.component.css'
 })
