2ddd3b1a3b5e7d72ecb3a4365d6cdbcf49d434d0
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-20 14:03:02 -0300

    component emp-update

---
+++ Front/src/app/components/emprestimos/emp-update/emp-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './emp-update.component.html',
+  styleUrl: './emp-update.component.css'
+})
+export class EmpUpdateComponent {
+
+}
