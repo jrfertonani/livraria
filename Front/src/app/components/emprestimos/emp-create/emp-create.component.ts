3d8875bfa1362fea2b29bcbbb542b3110ee93d8c
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-21 09:37:45 -0300

    implementando metodo create

--- Front/src/app/components/emprestimos/emp-create/emp-create.component.ts
+++ Front/src/app/components/emprestimos/emp-create/emp-create.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { EmpFormComponent } from "../emp-form/emp-form.component";
 import { Emprestimos } from '../../../model/Emprestimos';
+import { EmprestimosService } from '../../../services/emprestimos/emprestimos.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-emp-create',
@@ -12,8 +14,13 @@ import { Emprestimos } from '../../../model/Emprestimos';
 export class EmpCreateComponent {
 
 
-  createEmprestimo(Emprestimos: Emprestimos){
 
+  constructor(private serviceEmprestimo: EmprestimosService, private router: Router){}
+
+  createEmprestimo(emprestimos: Emprestimos){
+    this.serviceEmprestimo.createEmprestimo(emprestimos).subscribe((emprestimos)=>{
+      this.router.navigate(['/emprestimos'])
+    })
 
   }
 
