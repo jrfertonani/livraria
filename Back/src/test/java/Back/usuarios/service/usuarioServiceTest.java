fb5e5442dee68d5d6a5371865ef04226da201bb0
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-06 09:47:12 -0300

    usuarioService test

---
+++ Back/src/test/java/Back/usuarios/service/usuarioServiceTest.java
@@ -0,0 +1,33 @@
+package Back.usuarios.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class usuarioServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
