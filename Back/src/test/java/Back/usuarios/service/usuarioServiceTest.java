ce3f6f213133f1420b8abed2f4275f6dab017812
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-06 09:51:42 -0300

    usuarioService test

--- Back/src/test/java/Back/usuarios/service/usuarioServiceTest.java
+++ Back/src/test/java/Back/usuarios/service/usuarioServiceTest.java
@@ -1,11 +1,24 @@
 package Back.usuarios.service;
 
+import Back.usuarios.model.DTO.usuariosDTO;
+import Back.usuarios.model.entity.Usuarios;
+import Back.usuarios.repository.usuarioRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class usuarioServiceTest {
+
+    private usuarioService service;
+    private usuarioRepository repository;
+    private ModelMapper mapper;
+
+    private Usuarios usuarios;
+    private usuariosDTO usuariosDTO;
+    
+
 
     @BeforeEach
     void setUp() {
