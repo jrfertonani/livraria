1fa11214024ee4c70217e596a07512a05f9597a9
D M I <ademirjuniorfertonani@gmail.com>
2025-02-11 14:38:39 -0300

    emprestimos form

--- Front/src/app/components/emprestimos/emprestimo-form/emprestimo-form.component.ts
+++ Front/src/app/components/emprestimos/emprestimo-form/emprestimo-form.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-emprestimo-form',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './emprestimo-form.component.html',
   styleUrl: './emprestimo-form.component.css'
 })
-export class EmprestimoFormComponent {
+export class EmprestimoFormComponent implements OnInit {
+
+
+  emprestimoForm!: FormGroup;
+
+
+  ngOnInit(): void {
+    this.emprestimoForm = new FormGroup({
+      id: new FormControl(0),
+      livro: new FormControl(''),
+      usuario: new FormControl(''),
+      dataEmprestimo: new FormControl(''),
+      dataDevolucao: new FormControl(''),
+      devolvido: new FormControl(false)
+    });
+  }
 
 }
